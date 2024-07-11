fa2988ce2ab5d2e23fd2d053195fb0cd05acae5f
prajanravichandra <727722euit129@skcet.ac.in>
2024-07-11 10:57:30 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-   
+   <Home/>
+   <Header/>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export {};
+
+export const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
